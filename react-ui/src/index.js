a4f3d0569867950a4cff72e33421ad337086d07c
John (Juan) Lema <jlema@users.noreply.github.com>
2020-06-21 13:17:28 -0700

    removed service worker

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './registerServiceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'typeface-lobster';
 import 'typeface-open-sans';
