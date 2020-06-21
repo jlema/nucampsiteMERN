8c131b4e33ebca3cd299e446f1506910399a73a8
John (Juan) Lema <jlema@users.noreply.github.com>
2020-06-20 20:37:26 -0700

    initial commit

---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'typeface-lobster';
+import 'typeface-open-sans';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap-social/bootstrap-social.css';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '655971695196639',
+        clientSecret: '978df78242f088d3d323c6dd6211a953'
+    }
+}
