2b3500376543d9e1fa10cdffeab55cafecc1d9f6
John (Juan) Lema <jlema@users.noreply.github.com>
2020-06-21 13:19:18 -0700

    removed service worker correctly

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -11,4 +11,4 @@ import 'bootstrap-social/bootstrap-social.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// registerServiceWorker();
